f5258c9cc60f358d7a16e2e9d19c5548663dc1ea
freshaluck <freshaluck@localhost.localdomain>
2020-08-12 06:06:31 -0500

    reducer corrected

--- newproject/src/reducer/userReducer.js
+++ newproject/src/reducer/userReducer.js
@@ -13,45 +13,45 @@ const initialState = {
   token: localStorage.getItem('token'),
   isAuthenticated: null,
   isLoading: false,
-  user: null,
-}
+  user: null
+};
 
-export default function(state=initialState, action) {
-  switch(action.type) {
+export default function (state = initialState, action) {
+  switch (action.type) {
     case USER_LOADING:
       return {
         ...state,
-        isLoading: true,
-      }
+        isLoading: true
+      };
     case USER_LOADED:
       return {
         ...state,
-        isLoading: false,
         isAuthenticated: true,
+        isLoading: false,
         user: action.payload
-      }
+      };
     case LOGIN_SUCCESS:
     case REGISTER_SUCCESS:
-      localStorage.setItem('token', action.payload)
+      localStorage.setItem('token', action.payload.token)
       return {
         ...state,
         ...action.payload,
         isAuthenticated: true,
         isLoading: false
-      }
-    case LOGIN_FAIL:
-    case REGISTER_FAIL:
+      };
     case AUTH_ERROR:
+    case LOGIN_FAIL:
     case LOGOUT_SUCCESS:
-      localStorage.removeItem('token')
+    case REGISTER_FAIL:
+      localStorage.removeItem('token');
       return {
         ...state,
-        token: null, 
-        isAuthenticated: false,
+        token: null,
         user: null,
+        isAuthenticated: false,
         isLoading: false
       }
-    default: 
+    default:
       return state;
   }
 }
